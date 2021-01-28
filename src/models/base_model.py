4575c5506c89b7aa46a76dc8bee41a7ad8409d72
Derli Machado <dam.machado1@gmail.com>
2021-01-28 16:56:36 -0300

    Refactoring all tests.

--- src/models/base_model.py
+++ src/models/base_model.py
@@ -3,7 +3,8 @@ from sqlalchemy import Column, Integer
 
 Base = declarative_base()
 
-class BaseModel(Base):
-  __abstract__ = True
 
-  id = Column('id', Integer, primary_key=True)
+class BaseModel(Base):
+    __abstract__ = True
+
+    id = Column('id', Integer, primary_key=True)
