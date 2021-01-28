d8dc8ce29bcf8f51e4ec48b0a980400fd21b744a
Derli Machado <dam.machado1@gmail.com>
2021-01-28 15:21:50 -0300

    Added base_model

---
+++ src/models/base_model.py
@@ -0,0 +1,9 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer
+
+Base = declarative_base()
+
+class BaseModel(Base):
+  __abstract__ = True
+
+  id = Column('id', Integer, primary_key=True)
