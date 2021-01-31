7e859b0f28b15ae329f152b7714ab460dc3d2256
Matheus Abreu <matheus.abreu@olist.com>
2021-01-31 20:27:20 -0300

    Added additional exception asserts

--- tests/models/test_category.py
+++ tests/models/test_category.py
@@ -1,13 +1,11 @@
-from src.models.category import Category
-from src.models.base_model import BaseModel
-
 import pytest
 
+from src.models.base_model import BaseModel
+from src.models.category import Category
 
-def test_category_model():
-    name = 'cat0001'
-    description = 'desc0001'
 
+@pytest.mark.parametrize('name,description', [('cat0001', 'desc0001'), ('A' * 100, 'B' * 200)])
+def test_category_model(name, description):
     category = Category(name, description)
 
     assert isinstance(category, Category)
@@ -17,30 +15,36 @@ def test_category_model():
 
 
 def test_name_is_not_null():
-    with pytest.raises(ValueError):
-        category = Category(None, 'desc001')
+    with pytest.raises(TypeError) as exc:
+        Category(None, 'desc001')
+        assert 'must be str' in exc.value
 
 
 def test_name_is_str():
-    with pytest.raises(TypeError):
-        category = Category(0.1, 'desc002')
+    with pytest.raises(TypeError) as exc:
+        Category(0.1, 'desc002')
+        assert 'must be str' in exc.value
 
 
 def test_empty_name():
-    with pytest.raises(ValueError):
-        category = Category('', 'desc003')
+    with pytest.raises(ValueError) as exc:
+        Category('', 'desc003')
+        assert 'cannot be empty' in exc.value
 
 
 def test_name_len():
-    with pytest.raises(ValueError):
-        category = Category('x' * 201, 'desc004')
+    with pytest.raises(ValueError) as exc:
+        Category('x' * 201, 'desc004')
+        assert 'cannot have more than' in exc.value
 
 
 def test_description_is_str():
-    with pytest.raises(TypeError):
-        category = Category('name', 1.0)
+    with pytest.raises(TypeError) as exc:
+        Category('name', 1.0)
+        assert 'must be str' in exc.value
 
 
 def test_description_len():
-    with pytest.raises(ValueError):
-        category = Category('name', 'desc005' * 500)
+    with pytest.raises(ValueError) as exc:
+        Category('name', 'desc005' * 500)
+        assert 'cannot have more than' in exc.value
